0f13728879dce7b4fbfe957eb6b8fece6aab737a
子明 <37722993+ipuppet@users.noreply.github.com>
2020-10-15 22:17:58 +0800

    去掉无用函数

--- scripts/ui/widget/Schedule/index.js
+++ scripts/ui/widget/Schedule/index.js
@@ -16,30 +16,6 @@ class CalendarWidget {
         return await this.schedule.scheduleView()
     }
 
-    /**
-     * 将会作为函数传递
-     * @param {CallableFunction} view 视图处理函数
-     */
-    setTimeLine(view) {
-        let switchInterval = 1000 * 60 * 10 // 10分钟
-        let nowDate = new Date()
-        const expireDate = new Date(nowDate + switchInterval)
-        $widget.setTimeline({
-            entries: [
-                {
-                    date: nowDate,
-                    info: {}
-                }
-            ],
-            policy: {
-                afterDate: expireDate
-            },
-            render: ctx => {
-                return view(ctx)
-            }
-        })
-    }
-
     async render() {
         let switchInterval = 1000 * 60 * 10 // 10分钟
         let nowDate = new Date()
