ba783d43b6218c25d95717d0979c7b04649a20f5
子明 <37722993+ipuppet@users.noreply.github.com>
2020-10-27 22:33:13 +0800

    新增备份功能

--- scripts/app.js
+++ scripts/app.js
@@ -14,6 +14,8 @@ class AppKernel extends Kernel {
         // 小组件根目录
         this.widgetRootPath = "/scripts/ui/widget"
         this.widgetAssetsPath = "/assets/widget"
+        // backup
+        this.backupPath = "/assets/backup"
         // 更新所有小组件缓存
         this.refreshWidgetCache()
         // 检查是否携带URL scheme
@@ -67,7 +69,113 @@ class AppKernel extends Kernel {
 
         this.setting.tips = () => {
             this.settingComponent.view.touchHighlight()
-            $ui.alert("什么都没有~")
+            $ui.alert("每个小组件中都有README文件，点击可以得到一些信息。")
+        }
+
+        this.setting.backupToICloud = () => {
+            this.settingComponent.view.start()
+            const backupAction = async () => {
+                // 保证目录存在
+                if (!$file.exists(this.backupPath)) $file.mkdir(this.backupPath)
+                try {
+                    // 打包压缩
+                    await $archiver.zip({
+                        directory: this.widgetRootPath,
+                        dest: `${this.backupPath}/widgets.zip`
+                    })
+                    await $archiver.zip({
+                        directory: this.widgetAssetsPath,
+                        dest: `${this.backupPath}/userdata.zip`
+                    })
+                    await $archiver.zip({
+                        paths: [`${this.backupPath}/widgets.zip`, `${this.backupPath}/userdata.zip`],
+                        dest: `${this.backupPath}/backup.zip`
+                    })
+                    // 用户选择保存位置
+                    $drive.save({
+                        data: $data({ path: `${this.backupPath}/backup.zip` }),
+                        name: `EasyWidgetBackup-${new Date().getTime()}.zip`,
+                        handler: () => {
+                            //删除压缩文件
+                            $file.delete(this.backupPath)
+                            this.settingComponent.view.done()
+                        }
+                    })
+                } catch (error) {
+                    this.settingComponent.view.cancel()
+                    console.log(error)
+                }
+            }
+            $ui.alert({
+                title: $l10n("BACKUP"),
+                message: $l10n("START_BACKUP") + "?",
+                actions: [
+                    {
+                        title: $l10n("OK"),
+                        handler: () => {
+                            backupAction()
+                        }
+                    },
+                    {
+                        title: $l10n("CANCEL"),
+                        handler: () => { this.settingComponent.view.cancel() }
+                    }
+                ]
+            })
+        }
+
+        this.setting.recoverFromICloud = () => {
+            this.settingComponent.view.start()
+            $drive.open({
+                handler: data => {
+                    // 保证目录存在
+                    if (!$file.exists(this.backupPath)) $file.mkdir(this.backupPath)
+                    $file.write({
+                        data: data,
+                        path: `${this.backupPath}/backup.zip`
+                    })
+                    // 解压
+                    $archiver.unzip({
+                        path: `${this.backupPath}/backup.zip`,
+                        dest: this.backupPath,
+                        handler: async () => {
+                            if ($file.exists(`${this.backupPath}/widgets.zip`) && $file.exists(`${this.backupPath}/userdata.zip`)) {
+                                try {
+                                    // 保证目录存在
+                                    $file.mkdir(`${this.backupPath}/widgets`)
+                                    $file.mkdir(`${this.backupPath}/userdata`)
+                                    // 解压
+                                    await $archiver.unzip({
+                                        path: `${this.backupPath}/widgets.zip`,
+                                        dest: `${this.backupPath}/widgets`
+                                    })
+                                    await $archiver.unzip({
+                                        path: `${this.backupPath}/userdata.zip`,
+                                        dest: `${this.backupPath}/userdata`
+                                    })
+                                    // 删除文件
+                                    $file.delete(`${this.backupPath}/backup.zip`)
+                                    $file.delete(`${this.backupPath}/userdata.zip`)
+                                    $file.delete(`${this.backupPath}/userdata.zip`)
+                                    // 恢复
+                                    $file.move({
+                                        src: `${this.backupPath}/widgets`,
+                                        dst: this.widgetRootPath
+                                    })
+                                    $file.move({
+                                        src: `${this.backupPath}/userdata`,
+                                        dst: this.widgetAssetsPath
+                                    })
+                                    this.settingComponent.view.done()
+                                } catch (error) {
+                                    this.settingComponent.view.cancel()
+                                    console.log(error)
+                                }
+                            }
+                        }
+                    })
+                }
+            })
         }
     }
 
