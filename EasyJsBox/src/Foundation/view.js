d7d0594d65557a6d5a89e4263c818c163b23e4a5
子明 <37722993+ipuppet@users.noreply.github.com>
2020-11-21 13:35:19 +0800

    优化UI

--- EasyJsBox/src/Foundation/view.js
+++ EasyJsBox/src/Foundation/view.js
@@ -123,35 +123,44 @@ class BaseView {
                                 }
                             }
                         },
-                        { // 返回按钮
-                            type: "button",
-                            props: {
-                                bgcolor: $color("clear"),
-                                symbol: "chevron.left",
-                                tintColor: this.linkColor,
-                                title: ` ${parent}`,
-                                titleColor: this.linkColor,
-                                font: $font("bold", 16)
-                            },
-                            layout: (make, view) => {
-                                make.left.inset(10)
-                                make.centerY.equalTo(view.super)
-                            },
-                            events: {
-                                tapped: () => { $ui.pop() }
-                            }
-                        },
                         {
-                            type: "label",
-                            props: {
-                                text: title,
-                                font: $font("bold", 17)
-                            },
+                            type: "view",
                             layout: (make, view) => {
-                                make.center.equalTo(view.super)
-                            }
-                        }
-                    ].concat(navButtons)
+                                make.top.equalTo(view.super.safeAreaTop)
+                                make.bottom.width.equalTo(view.super)
+                            },
+                            views: [
+                                { // 返回按钮
+                                    type: "button",
+                                    props: {
+                                        bgcolor: $color("clear"),
+                                        symbol: "chevron.left",
+                                        tintColor: this.linkColor,
+                                        title: ` ${parent}`,
+                                        titleColor: this.linkColor,
+                                        font: $font("bold", 16)
+                                    },
+                                    layout: (make, view) => {
+                                        make.left.inset(10)
+                                        make.centerY.equalTo(view.super)
+                                    },
+                                    events: {
+                                        tapped: () => { $ui.pop() }
+                                    }
+                                },
+                                {
+                                    type: "label",
+                                    props: {
+                                        text: title,
+                                        font: $font("bold", 17)
+                                    },
+                                    layout: (make, view) => {
+                                        make.center.equalTo(view.super)
+                                    }
+                                }
+                            ].concat(navButtons)
+                        },
+                    ]
                 }
             ]
         })
